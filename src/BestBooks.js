707c4d2d51cfb4541b02804965f18b6fb63e84ca
Benjamin Ibarra <loco.queens@gmail.com>
2021-06-22 14:50:08 -0700

    completed trellos for lab12

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -2,18 +2,46 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
+import axios from 'axios';
+import { withAuth0 } from '@auth0/auth0-react';
+import Carousel from 'react-bootstrap/Carousel';
+
 
 class MyFavoriteBooks extends React.Component {
+
+  componentDidMount = async () => {
+    const { getIdTokenClaims } = this.props.auth0;
+    let tokenClaims = await getIdTokenClaims();
+    const jwt = tokenClaims.__raw;
+
+    const config = {
+      headers: { "Authorization": `Bearer ${jwt}` }
+    };
+    const bookData = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/books`, config);
+    console.log(bookData);
+    this.setState({ bookData: bookData.data });
+  }
   render() {
-    return(
+    const { user } = this.props.auth0;
+    return (
       <Jumbotron>
         <h1>My Favorite Books</h1>
-        <p>
-          This is a collection of my favorite books
-        </p>
+        <h3>This is a collection of {user.name} favorite books</h3>
+        { this.state ?
+            <Carousel fade>
+          {
+            this.state.bookData.map( data =>
+              <Carousel.Item>
+              <Carousel.Caption>
+                <h3>{data.name}</h3>
+              </Carousel.Caption>
+            </Carousel.Item> )
+          }
+        </Carousel> : ''
+        }
       </Jumbotron>
-    )
+    );
   }
-}
+};
 
-export default MyFavoriteBooks;
+export default withAuth0(MyFavoriteBooks);
